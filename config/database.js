c4d1139588f1984d3c937acb67c7dcd415a08e7d
Francis Julian Addison <faddison@Franciss-MacBook.local>
2018-01-10 19:11:06 -0500

    setting up for Heroku

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = { mongoURI: 'mongodb://julian:woodships@ds251197.mlab.com:51197/photo-blog-prod'}
+} else {
+    module.exports = { mongoURI: 'mongodb://localhost/photo-blog'}
+}
--- db/connection.js
+++ db/connection.js
@@ -7,11 +7,13 @@ const mongoose = require('mongoose')
 //     mongoose.connect('mongodb://localhost/photo-blog', { useMongoClient: true });
 // }
 
+const dataBase = require('../config/database')
+
 const mongoUri = 'mongodb://localhost/photo-blog'
 
 mongoose
-  .connect(mongoUri, {useMongoClient: true})
-  .then(connection => console.log(`Connection established to db '${connection.db.databaseName}'`))
+  .connect(dataBase.mongoURI, {useMongoClient: true})
+  .then(connection => console.log(`Connection established to db`))
   .catch(connectionError => console.log('Connection failed!', connectionError))
 
 
