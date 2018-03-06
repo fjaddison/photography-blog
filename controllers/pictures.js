591bca5e432bc3f00016bb313664de5e571a266a
Francis Julian Addison <fjaddison33@gmail.com>
2018-03-06 14:55:41 -0500

    prepping to switch back to handlebars

--- controllers/pictures.js
+++ controllers/pictures.js
@@ -1,54 +1,51 @@
-const methodOverride = require('method-override')
-
-const mongoose  = require('../models/schema')
+const mongoose = require('../models/schema')
 // assign schema (defined in schema.js) to variable
 const Pictures = mongoose.model('Pictures') // file extention not required
 
-const Router    = require('express').Router()
-
+const Router = require('express').Router()
 
 // main page route. Would ideally like the /pictures route to be the index
 Router.get('/', (req, res) => {
-    Pictures.find({})
+  Pictures.find({})
     .then(picturesData => {
       res.render('pictures-index', {
         pictures: picturesData
       })
     })
     .catch(err => {
-      console.log(err);
-    });
+      console.log(err)
+    })
 })
 
 // routes to individual image
 Router.get('/:title', (req, res) => {
-    Pictures.findOne({title: req.params.title}).then(picture => {
-        res.render('pictures-show', picture)
-    })
+  Pictures.findOne({title: req.params.title}).then(picture => {
+    res.render('pictures-show', picture)
+  })
 })
 
 // submit new image
 Router.post('/', (req, res) => {
-    Pictures.create(req.body.pictures)
+  Pictures.create(req.body.pictures)
       .then(pictures => {
-        res.redirect(`/pictures`);
+        res.redirect(`/pictures`)
       })
       .catch(err => {
-        console.log(err);
-      });
+        console.log(err)
+      })
 })
 
 // update image name
 Router.put('/:title', (req, res) => {
-    Pictures.findOneAndUpdate({ title: req.params.title }, req.body.pictures, {
-        new: true
-      })
+  Pictures.findOneAndUpdate({ title: req.params.title }, req.body.pictures, {
+    new: true
+  })
         .then(pictures => {
-          res.redirect(`/pictures/${pictures.title}`);
+          res.redirect(`/pictures/${pictures.title}`)
         })
         .catch(err => {
-          console.log(err);
-        });
+          console.log(err)
+        })
 })
 
 // Router.delete('/:id', (req, res) => {
@@ -60,14 +57,13 @@ Router.put('/:title', (req, res) => {
 // })
 
 Router.delete('/:title', (req, res) => {
-    Pictures.findOneAndRemove({ title: req.params.title })
+  Pictures.findOneAndRemove({ title: req.params.title })
       .then(() => {
-        res.redirect('/pictures');
+        res.redirect('/pictures')
       })
       .catch(err => {
-        console.log(err);
-      });
+        console.log(err)
+      })
 })
-
 
 module.exports = Router
